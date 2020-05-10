b17d90aee8e1be5a084c04f438dacc092ab6b7dd
Omar <omar.makled@gmail.com>
2020-05-10 23:10:01 +0200

    new

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <audio v-if="question.audio" controls>
         <source :src="question.src" type="audio/x-m4a" />
       </audio>
-      <p>
+      <div>
         <b-icon v-if="question.check === true" icon="check"></b-icon>
         <b-icon v-if="question.check === false" icon="x"></b-icon>
         {{question.answer}}
@@ -27,7 +27,12 @@
           animated
         ></b-progress>
         <span class="badge bg-info text-white" v-if="question.note">{{question.note}}</span>
-      </p>
+        <ul v-if="question.links">
+          <li v-for="(link, index) in question.links" :key="index">
+            <a :href="link">{{link}}</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -39,68 +44,88 @@ export default {
     return {
       questions: [
         {
-          title: "Vuejs",
-          answer: "+3 Years",
-          progress: {
-            value: 90,
-            max: 100
-          }
+          title: `Prefers to take advantage of Eloquent and Laravel's other magic methods instead of relying on raw DB/PHP queries`,
+          check: true
         },
         {
-          title: "laravel",
-          answer: "+5 Years",
-          progress: {
-            value: 90,
-            max: 100
-          }
+          title: "You have excellent PHP skills, using the latest OOP features",
+          check: true
         },
         {
-          title: "JavaScript",
-          answer: "+5 Years",
-          progress: {
-            value: 80,
-            max: 100
-          }
+          title: `You love testing, TDD, continuous integration and deployment and can’t imagine coding any other way (or you’d love to learn). PHPUnit is your friend!`,
+          check: true
         },
         {
-          title: "MySql",
-          answer: "+5 Years",
-          progress: {
-            value: 80,
-            max: 100
-          }
+          title: `Skill and a passion for system administration (unix/linux), continuous integration & deployment and cloud management tools will be highly regarded`,
+          check: true
         },
         {
-          title: "Rest API",
-          answer: "+3 Years",
-          progress: {
-            value: 80,
-            max: 100
-          }
+          title: `You're a strong team player, you’re keen to have your voice heard while appreciating the opinions of others`,
+          check: true
         },
         {
-          title: "Git",
-          answer: "+5 Years",
-          progress: {
-            value: 80,
-            max: 100
-          }
+          title: `You’re organized and methodical, with experience working in an Agile team environment`,
+          check: true
         },
         {
-          title: "Bootstrap",
-          answer: "+5 Years",
-          progress: {
-            value: 90,
-            max: 100
-          }
+          title: `At least 2 years dealing with PHP. Knowledge and practical experience with PHP`,
+          check: true
         },
         {
-          title: "About",
-          answer: "http://omarmakled.me/"
+          title: "Know Jquery, Ajax, JavaScript",
+          check: true
         },
         {
-          title: "Screencasts",
-          answer: "https://vimeo.com/305335935"
+          title: "Strong knowing of OOP and patterns",
+          check: true
+        },
+        {
+          title: `You must know how to deal with PDO and Doctrine ORM(or similar).`,
+          check: true
+        },
+        {
+          title: `Experience in plain SQL.`,
+          check: true
+        },
+        {
+          title: `Knowing Laravel framework`,
+          check: true
+        },
+        {
+          title: `You must use git`,
+          check: true
+        },
+        {
+          title: `HTML5, CSS3 (bootstrap).`,
+          check: true
+        },
+        {
+          title: `Understand and support legacy code`,
+          check: true
+        },
+        {
+          title: ` Deal with nginx and know how setup you server.`,
+          check: true
+        },
+        {
+          title: `Work in a team.`,
+          check: true
+        },
+        {
+          title: `Examples`,
+          links: [
+            "https://aqarmap.com.eg/en/",
+            "https://safebid.xyz/",
+            "http://baytlibya.com/",
+            "http://157.230.22.211:8088/",
+            "https://vimeo.com/305335935",
+            "http://omarmakled.me/"
+          ]
+        },
+        {
+          title:
+            "It would also be great to know how long it took for you to complete the sample projects to get an idea of your speed of deployment",
+          answer: `Let's talk `
         }
       ]
     };
